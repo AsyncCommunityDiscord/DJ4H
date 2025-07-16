0d2eeab696d4cb5595d26f4c4b5dda61dd45aff3
Gamingdy <christgik12@gmail.com>
2025-07-16 21:35:40 +0200

    feat: add ability to set custom score to user

--- commands/cogs/configuration.py
+++ commands/cogs/configuration.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 from config import LOGGER
 from utils.database.dao.guilds import GuildsDao
+from utils.database.dao.users import UserDao
 
 
 def convert_time_to_seconds(time_str: str) -> int:
@@ -63,6 +64,32 @@ class Configuration(commands.Cog):
         LOGGER.info(
             f"Guild {ctx.guild.id} updated with channel {channel.id} and delay {converted_time} seconds."
         )
+
+    @slash_command()
+    @discord.default_permissions()
+    async def set(self, ctx, member: discord.Member, score: int):
+        """Set a user's score."""
+        if not ctx.guild:
+            await ctx.respond("This command can only be used in a server.")
+            return
+
+        user_dao = UserDao()
+        user = user_dao.get_user(member.id, ctx.guild.id)
+
+        if user is None:
+            user_dao.add_user(member.id, ctx.guild.id, score)
+            await ctx.respond(f"Set user {member.mention}'s score to {score}.")
+            LOGGER.info(
+                f"User {member.id} added to guild {ctx.guild.id} with score {score}."
+            )
+        else:
+            user_dao.update_user(member.id, ctx.guild.id, score)
+            await ctx.respond(
+                f"User {member.mention}'s score updated to {score}."
+            )
+            LOGGER.info(
+                f"User {member.id} updated in guild {ctx.guild.id} with score {score}."
+            )
 
 
 def setup(bot):
