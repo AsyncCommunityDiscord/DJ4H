2c47c8b5030fc86f82313e4ff0871473f5a83eaa
Mityno <pititcowboy@gmail.com>
2026-09-11 11:06:08 +0200

    fix: Remove extraneous await

--- utils/tasks/rngdle_daily_leaderboard.py
+++ utils/tasks/rngdle_daily_leaderboard.py
@@ -54,7 +54,7 @@ async def rngdle_daily_leaderboard_task(bot: discord.Bot) -> None:
         for user, score_col, rank in zip(users, scores, range(len(users))):
             score = int(score_col.score)
             number = int(score_col.number)
-            u = await RNGdleLeaderboardUser.create_user_instance(user, score, number, rank + 1)
+            u = RNGdleLeaderboardUser.create_user_instance(user, score, number, rank + 1)
             leaderboard_users.append(u)
 
         generated = await generator.generate_leaderboard(leaderboard_users)
